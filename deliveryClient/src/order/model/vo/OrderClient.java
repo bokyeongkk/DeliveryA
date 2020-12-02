171eba2514f5693538888cedaf2b5a46d3274718
gyeong127 <mamiee030@gmail.com>
2020-12-02 13:46:15 +0900

    modify orderDet VO and add orderClient VO

---
+++ deliveryClient/src/order/model/vo/OrderClient.java
@@ -0,0 +1,5 @@
+package order.model.vo;
+
+public class OrderClient {
+
+}
--- deliveryClient/src/order/model/vo/OrderDet.java
+++ deliveryClient/src/order/model/vo/OrderDet.java
@@ -3,6 +3,7 @@ package order.model.vo;
 public class OrderDet {
 	
 	private int ordDetNo;
+	private int ordDetOrdNo;
 	private int ordDetMenuNo;
 	private int ordDetCnt;
 	
@@ -10,32 +11,46 @@ public class OrderDet {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public OrderDet(int ordDetNo, int ordDetMenuNo, int ordDetCnt) {
+
+	public OrderDet(int ordDetNo, int ordDetOrdNo, int ordDetMenuNo, int ordDetCnt) {
 		super();
 		this.ordDetNo = ordDetNo;
+		this.ordDetOrdNo = ordDetOrdNo;
 		this.ordDetMenuNo = ordDetMenuNo;
 		this.ordDetCnt = ordDetCnt;
 	}
-	
+
 	public int getOrdDetNo() {
 		return ordDetNo;
 	}
+
 	public void setOrdDetNo(int ordDetNo) {
 		this.ordDetNo = ordDetNo;
 	}
+
+	public int getOrdDetOrdNo() {
+		return ordDetOrdNo;
+	}
+
+	public void setOrdDetOrdNo(int ordDetOrdNo) {
+		this.ordDetOrdNo = ordDetOrdNo;
+	}
+
 	public int getOrdDetMenuNo() {
 		return ordDetMenuNo;
 	}
+
 	public void setOrdDetMenuNo(int ordDetMenuNo) {
 		this.ordDetMenuNo = ordDetMenuNo;
 	}
+
 	public int getOrdDetCnt() {
 		return ordDetCnt;
 	}
+
 	public void setOrdDetCnt(int ordDetCnt) {
 		this.ordDetCnt = ordDetCnt;
 	}
 	
-
+	
 }
